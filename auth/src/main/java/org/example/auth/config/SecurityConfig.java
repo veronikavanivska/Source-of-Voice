aa9cf8199c30029a8ff138de192b32f1860db2f0
veronikavanivska <veronikavanivska2005@gmail.com>
2026-04-25 18:25:16 +0200

    "feat(auth) : add security config to auth"

---
+++ auth/src/main/java/org/example/auth/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package org.example.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/error").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+}
