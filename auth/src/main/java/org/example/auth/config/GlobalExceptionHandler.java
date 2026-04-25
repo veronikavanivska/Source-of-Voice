2ebf1fb1df0ac0ba54ff7ec8e6bb65d2e06dd64a
veronikavanivska <veronikavanivska2005@gmail.com>
2026-04-25 18:25:36 +0200

    "feat(auth) : add error handler to see the errors clearly"

---
+++ auth/src/main/java/org/example/auth/config/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package org.example.auth.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleIllegalArgument(IllegalArgumentException ex) {
+        return Map.of("error", ex.getMessage());
+    }
+}
